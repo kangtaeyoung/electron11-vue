db8b0482392a5e1c844d30d0b544b9a9c3a18c63
kangtaeyoung <tykang@dangamsoft.com>
2021-02-02 17:41:32 +0900

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+   
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+ 
+
+  export default {
+    name: 'my-project',
+     components: {
+       
+   },
+   data() {
+      return {
+        items :  [
+          {
+            name: "File",
+            menu: [{ name: "Open"}, {name: "New Window"}, {name: "Exit"}]
+          },
+          {
+            name: "Edit",
+            menu: [{ name: "Cut"}, {name: "Copy"}, {name: "Paste"}]
+          }
+        ]
+      }
+   
+   }
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
